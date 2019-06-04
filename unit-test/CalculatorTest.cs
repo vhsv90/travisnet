f5487bcf1bd0f48b980952d2c8ccc88499b4f908
Victor Sanchez <vhsv90@gmail.com>
2019-06-04 08:43:55 -0600

    Unit test methods added for Sum and Sub

--- unit-test/CalculatorTest.cs
+++ unit-test/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using services;
 using System;
 using Xunit;
 
@@ -5,9 +6,38 @@ namespace unit_test
 {
     public class CalculatorTest
     {
-        [Fact]
-        public void Test1()
+        public Calculator calculator;
+
+        public CalculatorTest()
         {
+            calculator = new Calculator();
         }
+
+        [Fact]
+        public void SumOperation()
+        {
+            int num1 = 10;
+            int num2 = 40;
+            
+            int resultValidated = 50;
+
+            var result = calculator.Sum(num1, num2);
+
+            Assert.True(result == resultValidated);
+        }
+
+        [Fact]
+        public void SubOperation()
+        {
+            int num1 = 40;
+            int num2 = 10;
+            
+            int resultValidated = 30;
+
+            var result = calculator.Sub(num1, num2);
+
+            Assert.True(result == resultValidated);
+        }
+
     }
 }
